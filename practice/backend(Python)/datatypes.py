b609ca9ac9ed1cc3080bad2265f60d14aa5db95e
kc-clintone <kayseeclintone@gmail.com>
2023-10-23 19:26:42 +0000

    datatypes

---
+++ practice/backend(Python)/datatypes.py
@@ -0,0 +1,23 @@
+# Numeric Types
+my_integer = 5
+my_float = 3.14
+my_complex = 3 + 4j
+
+# Sequence Types
+my_string = "Hello, Python!"
+my_list = [1, 2, 3]
+my_tuple = (1, 2, 3)
+
+# Mapping Type
+my_dict = {'name': 'John', 'age': 30}
+
+# Set Types
+my_set = {1, 2, 3}
+my_frozenset = frozenset([1, 2, 3])
+
+# Boolean Type
+is_true = True
+is_false = False
+
+# None Type
+my_none = None
