c5b5aa4c5acfce5f975c1e46adb5e0012d10a4ea
kc-clintone <kayseeclintone@gmail.com>
2023-10-23 19:29:15 +0000

    datatypes

---
+++ practice/backend(Python)/lists.py
@@ -0,0 +1,10 @@
+#creating lists
+my_list = [1, 2, 3, 4, 5]
+str_list = ["apple", "banana", "cherry"]
+mixed_list = [1, "apple", 3.14, [7, 8]]
+
+#accessing lists
+my_list = [1, 2, 3, 4, 5]
+print(my_list[0])  # Output: 1
+print(my_list[2])  # Output: 3
+
