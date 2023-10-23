25e499e011cd5a70524653dd36a55f6b5ba872ac
kc-clintone <kayseeclintone@gmail.com>
2023-10-23 19:51:15 +0000

    python keywords

---
+++ practice/backend(Python)/keywords.py
@@ -0,0 +1,13 @@
+#python keywords
+False       class       finally     is          return
+None        continue    for         lambda      try
+True        def         from        nonlocal    while
+and         del         global      not         with
+as          elif        if          or          yield
+assert      else        import      pass
+break       except      in          raise
+
+import keyword
+
+print(keyword.kwlist)
+
