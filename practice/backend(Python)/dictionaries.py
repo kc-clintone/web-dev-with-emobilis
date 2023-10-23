534b1a5d543b0ed618872b6c9ac1922689e18a83
kc-clintone <kayseeclintone@gmail.com>
2023-10-23 19:33:14 +0000

    dictionaries

---
+++ practice/backend(Python)/dictionaries.py
@@ -0,0 +1,14 @@
+#creating dictionaries
+my_dict = {"name": "John", "age": 30, "city": "New York"}
+
+#accessing the values
+my_dict = {"name": "kaysee", "age": 30, "city": "Nairobi"}
+print(my_dict["name"])  # Output: "John"
+print(my_dict["age"])   # Output: 30
+
+
+#modify them
+my_dict = {"name": "kc", "clintone": 30}
+my_dict["age"] = 21
+my_dict["country"] = "Kenya"
+del my_dict["name"] 
