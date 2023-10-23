805010e3e98da69d08b48849a46066db60d72526
kc-clintone <kayseeclintone@gmail.com>
2023-10-23 19:56:33 +0000

    modified one file

--- practice/backend(Python)/testing_a_module.py
+++ practice/backend(Python)/testing_a_module.py
@@ -6,3 +6,9 @@ print(result)  # Prints the result
 
 value = mymodule.my_variable  # Accesses the variable from the module
 print(value)
+
+#standard lib module (built-in modules)
+import math
+
+result = math.sqrt(25)  # Uses the sqrt() function from the math module
+print(result)  # Prints the square root of 25
