23fe93e0094ea5516f135dc444fab4b9d286f5ec
kc-clintone <kayseeclintone@gmail.com>
2023-10-23 19:55:28 +0000

    testin a python modules

---
+++ practice/backend(Python)/testing_a_module.py
@@ -0,0 +1,8 @@
+import mymodule
+
+mymodule.say_hello()  # Calls the say_hello() function
+result = mymodule.add(3, 5)  # Calls the add() function
+print(result)  # Prints the result
+
+value = mymodule.my_variable  # Accesses the variable from the module
+print(value)
