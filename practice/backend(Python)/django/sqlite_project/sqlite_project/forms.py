ba20bb1d1d97a01cbeba32c6e422c082ba3b3a23
kaysee <kayseeclintone@gmail.com>
2023-11-01 22:04:10 +0300

    modified some files

--- practice/backend(Python)/django/sqlite_project/sqlite_project/forms.py
+++ practice/backend(Python)/django/sqlite_project/sqlite_project/forms.py
@@ -1,8 +1,8 @@
 from django import forms
-from .models import Task
+from .models import Todo
 
-class TaskForm(forms.ModelForm):
+class TodoForm(forms.ModelForm):
     
     class Meta:
-        model = Task
+        model = Todo
         fields = ['title','description']
--- practice/backend(Python)/django/sqlite_project/sqlite_project/views.py
+++ practice/backend(Python)/django/sqlite_project/sqlite_project/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Task
-from .forms import TaskForm
+from .models import Todo
+from .forms import TodoForm
+
+# ---create a todo---
+def create_todo(request):
+    if request.method == 'post':
+        this_form=TodoForm(request.POST)
+        if this_form.is_valid():
+            this_form.save()
+            return redirect('/')
+    else:
+        this_form=TodoForm()
+    return render(request, 'forms.html',{'form': this_form})
+
+# ---read todos---
+def list_todos(request):
+    todos=Todo.objects.all()
+    return render(request, 'index.html', {'todos': todos})
+
+# ---update todos---
+def update_todo(request, _id):
+    todo=get_object_or_404(Todo, _id=id)
+    if request.method == 'post':
+        this_form=TodoForm(request.POST,instance=todo)
+        if this_form.is_valid():
+            this_form.save()
+            return ridirect('/')
+    else:
+        this_form=TodoForm(instance=todo)
+    return render(request, 'forms.html', {'form': this_form})
+
+# ---delete a todo---
+def delete_todo(request, _id):
+    todo=get_object_or_404(Todo, _id=id)
+    todo.delete()
