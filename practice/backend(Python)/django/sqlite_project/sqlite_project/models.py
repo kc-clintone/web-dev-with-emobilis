d4988d64e485a4e92f5a17f036e66b451bb20a1d
kaysee <kayseeclintone@gmail.com>
2023-11-01 21:36:32 +0300

    started a new project for sqlite tutorial

---
+++ practice/backend(Python)/django/sqlite_project/sqlite_project/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Todo(models.Model):
+
+    title=models.CharField(max_length=30, required=True)
+    description=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    class Meta:
+
+        verbose_name = 'Todo'
+        verbose_name_plural = 'Todos'
