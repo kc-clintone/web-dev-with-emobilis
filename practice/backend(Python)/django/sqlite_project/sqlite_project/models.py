e7b122f60c18df5d3ad0c2d5b52513fe8a7051dd
kaysee <kayseeclintone@gmail.com>
2023-11-02 09:19:59 +0300

    modified some files

--- practice/backend(Python)/django/sqlite_project/sqlite_project/models.py
+++ practice/backend(Python)/django/sqlite_project/sqlite_project/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Todo(models.Model):
 
-    title=models.CharField(max_length=30, required=True)
+    title=models.CharField(max_length=30)
     description=models.TextField()
     created_at=models.DateTimeField(auto_now_add=True)
     class Meta:
