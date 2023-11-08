8ccbec3bd37269d1f448f3da42212e9372b609c0
kc-clintone <kayseeclintone@gmail.com>
2023-11-08 12:05:03 +0000

    modified the image upload project

--- practice/backend(Python)/django/imageupload/imageupload/urls.py
+++ practice/backend(Python)/django/imageupload/imageupload/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.upload_image, name='uploadImg'),
+    path('view_images/', views.view_images, name='viewImages')
 ]
