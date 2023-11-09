1d19b68edb53d8fb897a269db6e309cd27bb91ae
kc-clintone <kayseeclintone@gmail.com>
2023-11-09 15:02:24 +0000

    mod [1]

---
+++ practice/backend(Python)/django/djangoprisma/djangoprisma/views.py
@@ -0,0 +1,12 @@
+from rest_framework import generics
+from .models import Course, Lesson
+from .serializers import CourseSerializer, LessonSerializer
+
+class CourseList(generics.ListCreateAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+class LessonList(generics.ListCreateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
