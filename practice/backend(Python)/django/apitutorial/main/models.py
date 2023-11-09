1d7b4d32fc03484b44beacff68762c7ebe979281
kc-clintone <kayseeclintone@gmail.com>
2023-11-09 13:30:00 +0000

    mod [2]

--- practice/backend(Python)/django/apitutorial/main/models.py
+++ practice/backend(Python)/django/apitutorial/main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+	item=models.CharField(max_length=30)
+	description=models.TextField()
+
+	def __str__(self):
+		return self.name
