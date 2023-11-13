0e05352e5ff56915469ec7a2c4b4b2038e8cd8d5
kc-clintone <kayseeclintone@gmail.com>
2023-11-13 06:23:16 +0000

    mod (11)

--- practice/backend(Python)/django/todoapp/todoapp/forms.py
+++ practice/backend(Python)/django/todoapp/todoapp/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import ToDo
 
-clsss TodosForm(forms.ModelForm):
+clsss TaskForm(forms.ModelForm):
 	class Meta:
 		model=ToDo
 		fields=['todo', 'description']
