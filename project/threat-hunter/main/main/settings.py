83354be81978834075b16b0bb04d89144c759806
kc-clintone <kayseeclintone@gmail.com>
2023-11-29 17:32:15 +0000

    mod 20

--- project/threat-hunter/main/main/settings.py
+++ project/threat-hunter/main/main/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #---my applications----
-    'app.api.AppConfig',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +59,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
