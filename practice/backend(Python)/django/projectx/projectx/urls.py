390b7b0730a4ef98aa9e5ac2fcfd2c0bfced710e
kc-clintone <kayseeclintone@gmail.com>
2023-11-29 21:55:14 +0000

    just some updates

--- practice/backend(Python)/django/projectx/projectx/urls.py
+++ practice/backend(Python)/django/projectx/projectx/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.home, name ='home'),
-    path("about/", views.about, name ='about'),
-    path("gallery/", views.gallery, name = 'gallery'),
-    path("others/", views.others, name = 'others'),
+    path('', include('app.urls')),
+    #path("", views.home, name ='home'),
+    #path("about/", views.about, name ='about'),
+    #path("gallery/", views.gallery, name = 'gallery'),
+    #path("others/", views.others, name = 'others'),
 ]
