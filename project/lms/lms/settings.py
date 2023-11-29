a53c4b13607583fd78b5c1225b8a8c88189791dd
kc-clintone <kayseeclintone@gmail.com>
2023-11-29 11:23:49 +0000

    forgot some stupi updates😉😉

--- project/lms/lms/settings.py
+++ project/lms/lms/settings.py
@@ -25,7 +25,13 @@ SECRET_KEY = 'django-insecure-awx@07@nh+bs52r-)$ufzt-y1+m1k72)i!ns_0hsj_j-=p%o&h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
 
 
 # Application definition
@@ -37,10 +43,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',
+    #my apps
+    'django.contrib.sites',
+    'api.apps.ApiConfig',
+    'rest_framework',
 ]
 
+SITE_ID = 1
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
