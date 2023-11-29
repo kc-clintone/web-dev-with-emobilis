6c0e24579fbffe3fa7fb017946b2b4ca9809fae0
kc-clintone <kayseeclintone@gmail.com>
2023-11-29 14:52:47 +0000

    mod 4

---
+++ project/threat-hunter/main/main/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from .forms import ExtensionUploadForm
+from .models import UploadedExtension
+
+def upload_extension(request):
+    if request.method == 'POST':
+        form = ExtensionUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            # Add bug detection logic here
+            return redirect('result_page')
+    else:
+        form = ExtensionUploadForm()
+
+    return render(request, 'upload_extension.html', {'form': form})
+
