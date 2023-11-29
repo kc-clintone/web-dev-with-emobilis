99b8f52d7dc0d1d9a13e6d8eebcbf7f216e545e5
kc-clintone <kayseeclintone@gmail.com>
2023-11-29 16:08:06 +0000

    mod 8

--- project/threat-hunter/main/app/urls.py
+++ project/threat-hunter/main/app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-
+from . import views
 urlpatterns = [
-
+    path('', views.home, name='home'),
+    path('', views.signup, name='signup'),
+    path('', views.signin, name='signin'),
+    path('', views.dashboard, name='dashboard'),
 ]
