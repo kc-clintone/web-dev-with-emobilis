b73d6cd2d4019016cf41cc31137acbebd1efed62
kc-clintone <kayseeclintone@gmail.com>
2023-11-29 16:26:20 +0000

    mod 13

--- project/threat-hunter/main/app/urls.py
+++ project/threat-hunter/main/app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name=''),
     path('', views.signup, name='signup'),
     path('', views.signin, name='signin'),
     path('', views.dashboard, name='dashboard'),
--- project/threat-hunter/main/app/views.py
+++ project/threat-hunter/main/app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 def home(request):
-    pass
+    return HttpResponse("this is home")
 
 def dashboard(request):
     pass
