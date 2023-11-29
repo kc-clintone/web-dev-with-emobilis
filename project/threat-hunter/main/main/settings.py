fe865dca7bbf80f1c705d69a41428726c02b11ac
kc-clintone <kayseeclintone@gmail.com>
2023-11-29 16:16:25 +0000

    mod 10

--- project/threat-hunter/main/main/settings.py
+++ project/threat-hunter/main/main/settings.py
@@ -32,14 +32,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'crispy_forms',
-    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #---my applications----
+    'app.api.AppConfig',
 ]
 
 MIDDLEWARE = [
