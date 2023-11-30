c0e23bdec67085ffc282da3b4a598e872f8053b6
kc-clintone <kayseeclintone@gmail.com>
2023-11-30 07:37:41 +0000

    general commit 7

--- project/threat-hunter/app/forms.py
+++ project/threat-hunter/app/forms.py
@@ -4,4 +4,4 @@ from django.conrrib.auth.models import User
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
-        fielsd = ['username, 'email' 'password1', password2']
+        fielsd = ['username', 'email', 'password1', 'password2']
